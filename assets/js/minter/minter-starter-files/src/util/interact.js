6a195a6316c477bbf98bbd65f1112b6a1925ed0d
yuko <yuko.dev@gmail.com>
2021-09-06 14:45:06 +0300

    working sample, implement upload media to the pinata network

--- assets/js/minter/minter-starter-files/src/util/interact.js
+++ assets/js/minter/minter-starter-files/src/util/interact.js
@@ -1,91 +1,93 @@
-/*import { pinJSONToIPFS } from "./pinata.js";
+import {pinJSONToIPFS,pinFileToIPFS} from "./pinata.js";
+
 require("dotenv").config();
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const contractABI = require("../contract-abi.json");
 const contractAddress = "0x4C4a07F737Bf57F6632B6CAB089B78f62385aCaE";
-const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(alchemyKey);*/
+const {createAlchemyWeb3} = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+//const fs = require('fs');
 
 export const connectWallet = async () => {
-  if (window.ethereum) {
-    try {
-      const addressArray = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      const obj = {
-        status: "👆🏽 Write a message in the text-field above.",
-        address: addressArray[0],
-      };
-      return obj;
-    } catch (err) {
-      return {
-        address: "",
-        status: "😥 " + err.message,
-      };
-    }
-  } else {
-    return {
-      address: "",
-      status: (
-        <span>
+    if (window.ethereum) {
+        try {
+            const addressArray = await window.ethereum.request({
+                method: "eth_requestAccounts",
+            });
+            const obj = {
+                status: "👆🏽 Write a message in the text-field above.",
+                address: addressArray[0],
+            };
+            return obj;
+        } catch (err) {
+            return {
+                address: "",
+                status: "😥 " + err.message,
+            };
+        }
+    } else {
+        return {
+            address: "",
+            status: (
+                <span>
           <p>
             {" "}
-            🦊{" "}
-            <a target="_blank" href={`https://metamask.io/download.html`}>
+              🦊{" "}
+              <a target="_blank" href={`https://metamask.io/download.html`}>
               You must install Metamask, a virtual Ethereum wallet, in your
               browser.
             </a>
           </p>
         </span>
-      ),
-    };
-  }
+            ),
+        };
+    }
 };
 
 export const getCurrentWalletConnected = async () => {
-  if (window.ethereum) {
-    try {
-      const addressArray = await window.ethereum.request({
-        method: "eth_accounts",
-      });
-      if (addressArray.length > 0) {
+    if (window.ethereum) {
+        try {
+            const addressArray = await window.ethereum.request({
+                method: "eth_accounts",
+            });
+            if (addressArray.length > 0) {
+                return {
+                    address: addressArray[0],
+                    status: "👆🏽 Write a message in the text-field above.",
+                };
+            } else {
+                return {
+                    address: "",
+                    status: "🦊 Connect to Metamask using the top right button.",
+                };
+            }
+        } catch (err) {
+            return {
+                address: "",
+                status: "😥 " + err.message,
+            };
+        }
+    } else {
         return {
-          address: addressArray[0],
-          status: "👆🏽 Write a message in the text-field above.",
-        };
-      } else {
-        return {
-          address: "",
-          status: "🦊 Connect to Metamask using the top right button.",
-        };
-      }
-    } catch (err) {
-      return {
-        address: "",
-        status: "😥 " + err.message,
-      };
-    }
-  } else {
-    return {
-      address: "",
-      status: (
-        <span>
+            address: "",
+            status: (
+                <span>
           <p>
             {" "}
-            🦊{" "}
-            <a target="_blank" href={`https://metamask.io/download.html`}>
+              🦊{" "}
+              <a target="_blank" href={`https://metamask.io/download.html`}>
               You must install Metamask, a virtual Ethereum wallet, in your
               browser.
             </a>
           </p>
         </span>
-      ),
-    };
-  }
+            ),
+        };
+    }
 };
 
 async function loadContract() {
-  return new web3.eth.Contract(contractABI, contractAddress);
+    return new web3.eth.Contract(contractABI, contractAddress);
 }
 
 export const mintNFT = async (url, name, description) => {
@@ -96,46 +98,101 @@ export const mintNFT = async (url, name, description) => {
     };
   }
 
-  //make metadata
-  const metadata = new Object();
-  metadata.name = name;
-  metadata.image = url;
-  metadata.description = description;
+    //make metadata
+    const metadata = new Object();
+    metadata.name = name;
+    metadata.image = url;
+    metadata.description = description;
 
-  const pinataResponse = await pinJSONToIPFS(metadata);
-  if (!pinataResponse.success) {
-    return {
-      success: false,
-      status: "😢 Something went wrong while uploading your tokenURI.",
+    const pinataResponse = await pinJSONToIPFS(metadata);
+    if (!pinataResponse.success) {
+        return {
+            success: false,
+            status: "😢 Something went wrong while uploading your tokenURI.",
+        };
+    }
+    const tokenURI = pinataResponse.pinataUrl;
+    //console.log(tokenURI);
+    window.contract = await new web3.eth.Contract(contractABI, contractAddress);
+
+    const transactionParameters = {
+        to: contractAddress, // Required except during contract publications.
+        from: window.ethereum.selectedAddress, // must match user's active address.
+        data: window.contract.methods
+            .mintNFT(window.ethereum.selectedAddress, tokenURI)
+            .encodeABI(),
     };
-  }
-  const tokenURI = pinataResponse.pinataUrl;
 
-  window.contract = await new web3.eth.Contract(contractABI, contractAddress);
+    try {
+        const txHash = await window.ethereum.request({
+            method: "eth_sendTransaction",
+            params: [transactionParameters],
+        });
+        return {
+            success: true,
+            status:
+                "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" +
+                txHash,
+        };
+    } catch (error) {
+        return {
+            success: false,
+            status: "😥 Something went wrong: " + error.message,
+        };
+    }
+};
 
-  const transactionParameters = {
-    to: contractAddress, // Required except during contract publications.
-    from: window.ethereum.selectedAddress, // must match user's active address.
-    data: window.contract.methods
-      .mintNFT(window.ethereum.selectedAddress, tokenURI)
-      .encodeABI(),
-  };
 
-  try {
-    const txHash = await window.ethereum.request({
-      method: "eth_sendTransaction",
-      params: [transactionParameters],
-    });
-    return {
-      success: true,
-      status:
-        "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" +
-        txHash,
+export const mintNFTFromSelectedFile = async (file, name, description) => {
+    if (!file || name.trim() == "" || description.trim() == "") {
+        return {
+            success: false,
+            status: "❗Please make sure all fields are completed before minting.",
+        };
+    }
+    let data = new FormData();
+    data.append('file', file);
+    //make metadata
+    const metadata = new Object();
+    metadata.name = name;
+    metadata.description = description;
+    const metadataJson = JSON.stringify(metadata);
+    data.append('pinataMetadata', metadataJson);
+    //console.log(data);
+    const pinataResponse = await pinFileToIPFS(data);
+    if (!pinataResponse.success) {
+        return {
+            success: false,
+            status: "😢 Something went wrong while uploading your tokenURI.",
+        };
+    }
+    const tokenURI = pinataResponse.pinataUrl;
+    console.log(tokenURI);
+    window.contract = await new web3.eth.Contract(contractABI, contractAddress);
+
+    const transactionParameters = {
+        to: contractAddress, // Required except during contract publications.
+        from: window.ethereum.selectedAddress, // must match user's active address.
+        data: window.contract.methods
+            .mintNFT(window.ethereum.selectedAddress, tokenURI)
+            .encodeABI(),
     };
-  } catch (error) {
-    return {
-      success: false,
-      status: "😥 Something went wrong: " + error.message,
-    };
-  }
+
+    try {
+        const txHash = await window.ethereum.request({
+            method: "eth_sendTransaction",
+            params: [transactionParameters],
+        });
+        return {
+            success: true,
+            status:
+                "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" +
+                txHash,
+        };
+    } catch (error) {
+        return {
+            success: false,
+            status: "😥 Something went wrong: " + error.message,
+        };
+    }
 };
--- assets/js/minter/minter-starter-files/src/util/pinata.js
+++ assets/js/minter/minter-starter-files/src/util/pinata.js
@@ -1,6 +1,10 @@
-require('dotenv').config();
+require('dotenv').config();//todo: it does not work with Encore, so env is configured in webpack.config.js
+//const Dotenv = require('dotenv-webpack');
 const key = process.env.REACT_APP_PINATA_KEY;
+//console.log(key);
 const secret = process.env.REACT_APP_PINATA_SECRET;
+//console.log(key);
+//console.log(secret);
 const axios = require('axios');
 
 export const pinJSONToIPFS = async(JSONBody) => {
@@ -24,6 +28,33 @@ export const pinJSONToIPFS = async(JSONBody) => {
                 success: false,
                 message: error.message,
             }
-           
+
+        });
+};
+export const pinFileToIPFS = async(data) => {
+    console.log(data);
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+    return axios
+        .post(url, data, {
+            maxBodyLength: 'Infinity', //this is needed to prevent axios from erroring out with large files
+            headers: {
+                'Content-Type': 'multipart/form-data; boundary=${data._boundary}',
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
+            }
+        })
+        .then(function (response) {
+           return {
+               success: true,
+               pinataUrl: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
         });
 };
